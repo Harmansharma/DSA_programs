9fa0cbd4678388db13778bf0fcf8a9195e198451
vinay <gnulinooks@gmail.com>
2024-02-18 10:03:11 +0530

    add switch case for integer and char

--- switch/switch.cpp
+++ switch/switch.cpp
@@ -8,22 +8,41 @@ int main(){
   cout << "Enter the  operation"<< endl;
   cin >> operation;
   float answer = 0;
-  if (operation == 0){
-    answer = firstNumber + secondNumber;
-  }
-  else if(operation == 1){
-    answer = firstNumber - secondNumber;
-  }
-  else if(operation == 2){
-    answer = firstNumber * secondNumber;
-  }
-  else if(operation == 3){
-    answer = firstNumber / secondNumber;
-  }
-  else 
-  {
-    cout << "invalid operation"<< endl;
-    return 0;
+  ////////////////////////////Long if else case implement//////////////////////////////
+  // if (operation == 0){
+  //   answer = firstNumber + secondNumber;
+  // }
+  // else if(operation == 1){
+  //   answer = firstNumber - secondNumber;
+  // }
+  // else if(operation == 2){
+  //   answer = firstNumber * secondNumber;
+  // }
+  // else if(operation == 3){
+  //   answer = firstNumber / secondNumber;
+  // }
+  // else
+  // {
+  //   cout << "invalid operation"<< endl;
+  //   return 0;
+  // }
+////////////////////////////////////SWITCH cases impliment/////////////////////////////////////
+  switch(operation){
+    case 1:
+      answer = firstNumber + secondNumber;
+      break;
+    case 2:
+      answer = firstNumber - secondNumber;
+      break;
+    case 3:
+      answer = firstNumber * secondNumber;
+      break;
+    case 4:
+      answer = firstNumber / secondNumber;
+      break;
+    default:
+      cout <<"default case"<< endl;
+      break;
   }
   cout << "answer is" << answer<< endl;
   return 0;
