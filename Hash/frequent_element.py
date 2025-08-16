423619cdeab7de2db61b1674707ee92d0acaef2d
Harmansharman <sharmaharman539@gmail.com>
2025-08-17 01:18:46 +0530

    push hash first code

---
+++ Hash/frequent_element.py
@@ -0,0 +1,32 @@
+arr = [2,2,2,2,3,1,1,1,1,7,7,7,7]
+freq = {}
+for num in arr:
+    if num in freq:
+        freq[num]+=1
+    else:
+        freq[num] = 1
+print(freq)
+maxi = 0
+maxi_key = float('inf')
+
+for i, j in freq.items():
+    if j > maxi:   # new bigger value
+        maxi = j
+        maxi_key = i
+    elif j == maxi and i < maxi_key:  # same value, pick smaller key
+        maxi_key = i
+        
+        
+        
+    
+print(maxi)
+print(maxi_key)
+
+
+
+
+#   if v > max_val:
+#         max_val = v
+#         max_key = k
+#     elif v == max_val and k > max_key:  # pick bigger key if tie
+#         max_key = k   this code for max key
