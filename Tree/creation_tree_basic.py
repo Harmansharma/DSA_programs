6d5070e65cf0d63917ca78e03308062abe50d73d
Harmansharman <sharmaharman539@gmail.com>
2025-08-19 08:31:55 +0530

    add traversal

--- Tree/creation_tree_basic.py
+++ Tree/creation_tree_basic.py
@@ -6,20 +6,48 @@ class Tree:
     
     
 def create_tree():
-        data = int(input("Enter the input:"))
-        if data == -1:
-            return None
-        root = Tree(data)
-        print("Enter the left child")
-        root.left = create_tree()
-        print("enter the right child ")
-        root.right = create_tree()
-        
-        return root
-        
+    data = int(input("Enter the input:"))
+    if data == -1:
+        return None
+    root = Tree(data)
+    print("Enter the left child")
+    root.left = create_tree()
+    print("enter the right child ")
+    root.right = create_tree()
+    return root
+
+def preorder(root):
+    if root is None:
+        return
+    print(root.data, end= " ")
+    print()
+    preorder(root.left)
+    preorder(root.right)
+    
+def inorder(root):
+    if root is None:
+        return
+    inorder(root.left)
+    print(root.data, end= " ")
+    inorder(root.right)
+    
+def postorder(root):
+    if root is None:
+        return
+    inorder(root.left)
+    inorder(root.right)
+    print(root.data, end =" ")
+
 print("create tree: ")    
 firstNode = create_tree()
+print("\nPreorder Traversal:")
+preorder(firstNode)
 
+print("\n\nInorder Traversal:")
+inorder(firstNode)
+
+print("\n\nPostorder Traversal:")
+postorder(firstNode)
 # secondNode = Tree(3)
 # thirdNode = Tree(4)
 # forthNode = Tree(5)
